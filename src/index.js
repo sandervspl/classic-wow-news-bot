26c744ccd93270f843ecb7869192a62aea6bd686
Sander Vispoel <contact@sandervispoel.com>
2019-04-09 22:45:00 +0200

    Update rss feed fetch

--- src/index.js
+++ src/index.js
@@ -1,21 +1,49 @@
-var Parser = require('rss-parser');
-var parser = new Parser();
+let fetch = require('node-fetch');
+let Parser = require('rss-parser');
+let parser = new Parser();
 
-setInterval(async () => {
+let lastUpdateId = '';
+
+async function getFeed() {
   let feed = await parser.parseURL('https://www.reddit.com/r/classicwow/.rss');
   console.log('--- ' + feed.title + ' ---');
 
-  feed.items.forEach((item) => {
+  feed.items.forEach(item => {
     if (item.content.includes('us.forums.blizzard.com')) {
+      console.log(1);
+      if (item.id === lastUpdateId) return;
+
       const url = item.content.match(
         /https:\/\/us.forums.blizzard.com\/en\/wow\/[t/a-z-]+\d+/
       );
 
-      if (url) {
-        console.log(item.id);
-        console.log(item.title);
-        console.log(url[0]);
-      }
+      console.log(2);
+      if (!url) return;
+
+      console.log(item.id);
+      console.log(item.title);
+      console.log(url[0]);
+
+      fetch(
+        'https://discordapp.com/api/webhooks/561859993587154954/to8nVsewGm94K431aKCw-bI-dtfxAorby8M5xAyaBfu3p5st0inY6OxGgQXKu7KBg9lH',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: {
+            content: ':warning: @here Classic news from Blizzard!',
+            embeds: [
+              { title: '{{EntryTitle}}', url: ' {{EntryUrl}}' },
+              { image: { url: '{{EntryImageUrl}}' } },
+            ],
+          },
+        }
+      );
     }
   });
-}, 30000);
+}
+
+setInterval(getFeed, 30000);
+
+getFeed();
